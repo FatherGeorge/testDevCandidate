f213836d53aedaf90714837d1b2c5ad4fa76c7c9
friend <friendly@friend.me>
2018-05-03 17:41:16 +0300

    - persistence api

--- src/main/java/org/parts/dao/PartsDAOImpl.java
+++ src/main/java/org/parts/dao/PartsDAOImpl.java
@@ -3,8 +3,6 @@ package org.parts.dao;
 import org.parts.model.Part;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -13,19 +11,19 @@ import java.util.List;
 @Repository
 public class PartsDAOImpl implements PartsDAO {
 
-    @PersistenceContext
-    private EntityManager em;
+/*    @PersistenceContext
+    private EntityManager em;*/
 
-/*    private List<Part> parts = new ArrayList<>();
+    private List<Part> parts = new ArrayList<>();
     {
         parts.add(new Part("10", "Flame thrower" ,"factory", 10, new GregorianCalendar(2017, 4, 7).getTime(), new GregorianCalendar(2017, 5, 6).getTime()));
         parts.add(new Part("50", "Rocket launcher" ,"factory", 15, new GregorianCalendar(2016, 4, 7).getTime(), new GregorianCalendar(2012, 9, 7).getTime()));
         parts.add(new Part("60", "Gas splitter" ,"factory", 18, new GregorianCalendar(2017, 3, 8).getTime(), new GregorianCalendar(2018, 5, 7).getTime()));
-    }*/
+    }
 
     @Override
     public List<Part> getAllParts() {
-/*        List<Part> clonedParts = new ArrayList<>();
+        List<Part> clonedParts = new ArrayList<>();
         for(Part part : parts){
             Part clonedPart = new Part(part.getPartNumber(),
                                        part.getPartName(),
@@ -34,9 +32,9 @@ public class PartsDAOImpl implements PartsDAO {
                                        new Date(part.getShipped().getTime()),
                                        new Date(part.getReceived().getTime()));
             clonedParts.add(clonedPart);
-        }*/
-        /*return clonedParts;*/
+        }
+        return clonedParts;
 
-        return em.createQuery("from PARTS", Part.class).getResultList();
+        /*return em.createQuery("from PARTS", Part.class).getResultList();*/
     }
 }
